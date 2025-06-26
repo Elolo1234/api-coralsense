95054f5c380e45a9ffd233e9bd394b84841918e2
eloisa.c@aluno.ifsp.edu.br <eloisa.c@aluno.ifsp.edu.br>
2025-06-26 19:00:29 -0300

    pesquisador_service

--- API_coralsense/services/pesquisador_service.py
+++ API_coralsense/services/pesquisador_service.py
@@ -3,13 +3,18 @@ import os
 
 CAMINHO_PESQUISADOR_JSON = os.path.join(os.path.dirname(__file__), '..', 'dados', 'pesquisadores.json')
 
-def carregar_pesquisadores_json():
+def inicializar_arquivo():
+    os.makedirs(os.path.dirname(CAMINHO_PESQUISADOR_JSON), exist_ok=True)
+    if not os.path.exists(CAMINHO_PESQUISADOR_JSON):
+        with open(CAMINHO_PESQUISADOR_JSON, "w", encoding="utf-8") as f:
+            json.dump([], f)
 
+def carregar_pesquisadores_json():
+    inicializar_arquivo()
     try:
         with open(CAMINHO_PESQUISADOR_JSON, "r", encoding="utf-8") as f:
             return json.load(f)
-    except FileNotFoundError:
-        return []
+        
     except json.JSONDecodeError:
         return []
 
@@ -19,7 +24,7 @@ def salvar_pesquisadores_json(lista_pesquisadores):
         json.dump(lista_pesquisadores, f, indent=4, ensure_ascii=False)
 
 def adicionar_pesquisador_json(pesquisador):
-    
+
     pesquisadores = carregar_pesquisadores_json()
     pesquisador["id"] = len(pesquisadores) + 1
     pesquisadores.append(pesquisador)
@@ -27,18 +32,18 @@ def adicionar_pesquisador_json(pesquisador):
     return pesquisador
 
 def listar_pesquisadores_json():
-   
+
     return carregar_pesquisadores_json()
 
 def buscar_pesquisador_por_id_json(id):
-    
-    for p in carregar_pesquisadores_json():
+    pesquisadores = carregar_pesquisadores_json()
+    for p in pesquisadores:
         if p["id"] == id:
             return p
     return None
 
 def atualizar_pesquisador_json(id, dados_atualizados):
-   
+
     pesquisadores = carregar_pesquisadores_json()
     for i, p in enumerate(pesquisadores):
         if p["id"] == id:
@@ -48,7 +53,7 @@ def atualizar_pesquisador_json(id, dados_atualizados):
     return None
 
 def deletar_pesquisador_por_id_json(id):
-   
+    
     pesquisadores = carregar_pesquisadores_json()
     nova_lista = [p for p in pesquisadores if p["id"] != id]
     if len(nova_lista) < len(pesquisadores):
