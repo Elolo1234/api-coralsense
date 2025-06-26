4175457f25985dcd04d51b7ffdb485d97f957c48
eloisa <eloisa.c@aluno.ifsp.edu.br>
2025-06-26 11:34:23 -0300

    atualizar pesquisa_services

--- API_coralsense/services/pesquisa_service.py
+++ API_coralsense/services/pesquisa_service.py
@@ -1,23 +1,59 @@
-from models.pesquisa import Pesquisa
+import json
+import os
 
-def listar_pesquisas():
-    pesquisas = Pesquisa.query.all()
-    return [p.to_dict() for p in pesquisas]
 
-def criar_pesquisa(dados):
-    nova = Pesquisa(**dados)
-    db_session.add(nova)
-    db_session.commit()
-    return nova.to_dict()
+CAMINHO_PESQUISA_JSON = "dados/pesquisas.json"
 
-def atualizar_pesquisa(titulo, dados):
-    pesquisa = Pesquisa.query.get_or_404(titulo)
-    for chave, valor in dados.items():
-        setattr(pesquisa, chave, valor)
-    db_session.commit()
-    return pesquisa.to_dict()
 
-def deletar_pesquisa(titulo):
-    pesquisa = Pesquisa.query.get_or_404(titulo)
-    db_session.delete(pesquisa)
-    db_session.commit()
+
+
+def carregar_pesquisas_json():
+    try:
+        with open(CAMINHO_PESQUISA_JSON, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
+    except json.JSONDecodeError:
+        return []
+
+def salvar_pesquisas_json(lista_pesquisas):
+    with open(CAMINHO_PESQUISA_JSON, "w", encoding="utf-8") as f:
+        json.dump(lista_pesquisas, f, indent=4, ensure_ascii=False)
+
+def adicionar_pesquisa_json(pesquisa):
+    
+    pesquisas = carregar_pesquisas_json()
+    pesquisa["id"] = len(pesquisas) + 1
+    pesquisas.append(pesquisa)
+    salvar_pesquisas_json(pesquisas)
+    return pesquisa
+
+def listar_pesquisas_json():
+   
+    return carregar_pesquisas_json()
+
+def buscar_pesquisa_por_id_json(id):
+  
+    for p in carregar_pesquisas_json():
+        if p["id"] == id:
+            return p
+    return None
+
+def atualizar_pesquisa_json(id, dados_atualizados):
+    
+    pesquisas = carregar_pesquisas_json()
+    for i, p in enumerate(pesquisas):
+        if p["id"] == id:
+            pesquisas[i].update(dados_atualizados)
+            salvar_pesquisas_json(pesquisas)
+            return pesquisas[i]
+    return None
+
+def deletar_pesquisa_por_id_json(id):
+  
+    pesquisas = carregar_pesquisas_json()
+    nova_lista = [p for p in pesquisas if p["id"] != id]
+    if len(nova_lista) < len(pesquisas):
+        salvar_pesquisas_json(nova_lista)
+        return True
+    return False
